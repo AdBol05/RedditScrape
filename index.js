6d7f622411507197bc6cc2647c6678c3cf4c5ddd
AdBol05 <adam.b@volny.cz>
2023-03-08 09:03:13 +0100

    https

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const https = require("https");
+
+let source = process.argv.slice(2)[0];
+let options = {
+    host: "www.reddit.com",
+    path: source,
+    headers: {"user-agent": "request"}
+};
+
 let urls = [];
 
-console.table(urls);
+console.log("Input url: " + options.host + source + "\nRetrieved links:");
+
+https.get(options, function (res) {
+    let json = '';
+    res.on('data', function (chunk) {
+        json += chunk;
+    });
+
+    res.on('end', function () {
+        if (res.statusCode === 200) {
+            try {
+                let data = JSON.parse(json);
+                
+                console.log(data.data.children);
+                console.table(urls);
+
+            } catch (e) {
+                console.log('Error parsing JSON!');
+            }
+        } else {
+            console.log('Status:', res.statusCode);
+        }
+    });
+    
+}).on('error', function (err) {
+      console.log('Error:', err);
+});
