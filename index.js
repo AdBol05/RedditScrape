c46062fec3b5726c66d34f8ea7ffef60bcde29f5
AdBol05 <adam.b@volny.cz>
2023-03-08 09:42:23 +0100

    input

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 const https = require("https");
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let source = process.argv.slice(2)[0];
+let source = "/r/";// + source + ".json"
+readline.question('Enter subreddit name: ', sub => {
+    source = source + sub + ".json";
+    readline.close();
 
-source = "/" + source + ".json"
+    let options = {
+        host: "www.reddit.com",
+        path: source,
+        headers: { "user-agent": "request" }
+    };
 
-let options = {
-    host: "www.reddit.com",
-    path: source,
-    headers: {"user-agent": "request"}
-};
+    let urls = [];
 
-let urls = [];
 
-console.log("Input url: " + options.host + source + "\nRetrieved links:");
+    console.log("Input url: " + options.host + source + "\nRetrieved links:");
 
-https.get(options, function (res) {
-    let json = '';
-    res.on('data', function (chunk) {
-        json += chunk;
-    });
+    https.get(options, function (res) {
+        let json = '';
+        res.on('data', function (chunk) {
+            json += chunk;
+        });
 
-    res.on('end', function () {
-        if (res.statusCode === 200) {
-            try {
-                let data = JSON.parse(json).data.children;
-                
-                //console.log(data);
-                for (i in data){
-                    urls.push(data[i].data.url);
+        res.on('end', function () {
+            if (res.statusCode === 200) {
+                try {
+                    let data = JSON.parse(json).data.children;
+
+                    //console.log(data);
+                    for (i in data) {
+                        urls.push(data[i].data.url);
+                    }
+                    console.table(urls);
+
+                } catch (e) {
+                    console.log('Error parsing JSON!');
                 }
-                console.table(urls);
-
-            } catch (e) {
-                console.log('Error parsing JSON!');
+            } else {
+                console.log('Status:', res.statusCode);
             }
-        } else {
-            console.log('Status:', res.statusCode);
-        }
+        });
+
+    }).on('error', function (err) {
+        console.log('Error:', err);
     });
-    
-}).on('error', function (err) {
-      console.log('Error:', err);
 });
