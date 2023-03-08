f83a93c6eacde9e0754404c47fd95c2664861bb5
AdBol05 <adam.b@volny.cz>
2023-03-08 17:51:53 +0100

    number of links

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const readline = require('readline').createInterface({
 });
 
 let source = "/r/";
-readline.question('Enter subreddit name and number of results <name> <number>: ', sub => {
+readline.question('Enter subreddit name and number of results [<name> <number>]: ', sub => {
     let args = sub.split(" ");
     if(args[1] === undefined){args[1] = "30"}
     source = source + args[0] + ".json?limit=" + args[1];
